6970191397f1e0501b46b3af113739fe257e7276
Cyril <cyril.bier42100@gmail.com>
2024-10-29 09:28:54 +0100

    Init commit

---
+++ src/main/java/com/cbi/BetOnTrack/BetOnTrackApplication.java
@@ -0,0 +1,13 @@
+package com.cbi.BetOnTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetOnTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetOnTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbi/BetOnTrack/BetOnTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbi.BetOnTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetOnTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
