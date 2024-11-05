ad626b2da5a8d53738fbfda349e8fd73d3b8e9ce
Cyril <cyril.bier42100@gmail.com>
2024-11-05 16:27:02 +0100

    POST competition in controller

--- src/main/java/com/cbi/BetOnTrack/controller/CompetitionController.java
+++ src/main/java/com/cbi/BetOnTrack/controller/CompetitionController.java
@@ -5,6 +5,8 @@ import com.cbi.BetOnTrack.dto.CreateCompetitionEvent;
 import com.cbi.BetOnTrack.dto.CreatePerformance;
 import com.cbi.BetOnTrack.model.Competition;
 import com.cbi.BetOnTrack.model.CompetitionEvent;
+import com.cbi.BetOnTrack.service.CompetitionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/competition")
 public class CompetitionController {
+    @Autowired
+    CompetitionService competitionService;
+
     @GetMapping
     public ResponseEntity<List<Competition>> getCompetition(
             @RequestParam(name = "ids",defaultValue = "") List<Long> ids
@@ -25,7 +30,7 @@ public class CompetitionController {
     public ResponseEntity<List<Competition>> postCompetition(
             @RequestBody List<CreateCompetition> competitions
     ){
-        return new ResponseEntity<>(null,HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>(competitionService.postCompetition(competitions),HttpStatus.NOT_IMPLEMENTED);
     }
 
     @PostMapping("/events")
